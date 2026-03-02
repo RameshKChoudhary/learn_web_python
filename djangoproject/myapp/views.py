069664e154a031ee4ac3b6a097c7633a449b3bd3
RameshKChoudhary <ramesh2005choudhary@gmail.com>
2026-03-02 16:29:26 +0000

    second day

--- djangoproject/myapp/views.py
+++ djangoproject/myapp/views.py
@@ -3,4 +3,13 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('<h1>hello</h1>')
+    # return HttpResponse('<h1>hello</h1>')
+    ###dynamic input
+    # name ='Ramesh'
+    # return render(request,'index.html',{'name':name})
+    # ##better use like this
+    context ={
+        'name':'Ramesh',
+        'age':20,
+    }
+    return render(request,'index.html',context)
